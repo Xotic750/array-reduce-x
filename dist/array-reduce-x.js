9d7f3f5dae41584abb6312e9f224a9a300c87247
Graham Fairweather <xotic750@gmail.com>
2019-07-26 00:29:58 +0200

    :bookmark: v3.0.12
    Auto build

--- dist/array-reduce-x.js
+++ dist/array-reduce-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-24T16:53:38.075Z",
+  "date": "2019-07-25T22:29:52.361Z",
   "describe": "",
   "description": "Reduce an array (from left to right) to a single value.",
   "file": "array-reduce-x.js",
   "hash": "a8937bed973d1b506703",
   "license": "MIT",
-  "version": "3.0.11"
+  "version": "3.0.12"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
