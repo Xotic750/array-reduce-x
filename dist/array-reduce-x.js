c78a9fa44f092b7bd295fa26a2a52965b3d01ecd
Graham Fairweather <xotic750@gmail.com>
2019-09-05 01:50:53 +0200

    Maintenance

--- dist/array-reduce-x.js
+++ dist/array-reduce-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-30T22:27:11.204Z",
+  "date": "2019-09-04T23:50:46.164Z",
   "describe": "",
   "description": "Reduce an array (from left to right) to a single value.",
   "file": "array-reduce-x.js",
